8b2bbc4b56f99498d08b7a3ce71694eae48334cb
Denis Ergin <denis.ergin@diva-e.com>
2024-10-18 11:52:19 +0200

    feat(knowledge-base): Build Web-Engineering-I introduction page with links to slides

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,6 +7,17 @@ const skillCollection = defineCollection({
   }),  
 });
 
+const dhbwCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    staticPath: z.string(),
+    show: z.optional(z.boolean()),
+  }),
+});
+
 export const collections = {
   'skills': skillCollection,
+  'dhbw': dhbwCollection,
 }
---
+++ src/content/dhbw/de/web-engineering-ii.md
@@ -0,0 +1,14 @@
+---
+title: "Web Engineering II"
+tags: 
+- "dhbw"
+- "web"
+- "engineering"
+- "html"
+- "css"
+- "javascript"
+slug: "web-engineering-ii"
+staticPath: "knowledge-base/dhbw/"
+show: false
+---
+# Hallo Welt!
